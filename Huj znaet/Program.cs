8ec1643f211f1b3be9971e3d0299813d3fb67fed
SOUPEC <frybenderer43@gmail.com>
2024-10-23 10:41:59 +0200

    Idi ti

--- Huj znaet/Program.cs
+++ Huj znaet/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using NAudio.Wave;
+class Program{
+public static void Main(){
+   
+    Console.WriteLine("ONE!");
+    Console.WriteLine("WHAT COMES AFTER ONE?!");
+    Console.WriteLine("TWO!");
+    Console.WriteLine("WHAT COMES AFTER TWO ?!");
+    Console.WriteLine("THREE!");
+    Console.WriteLine("WHAT COMES THREE?!");
+    Console.WriteLine("FOOOUUUURRR!!!!!");
+}
+}
